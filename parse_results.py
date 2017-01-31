2fe99a4bac2ea61676c6ed14ff5aae7fcfc14219
mikitski <github@nekit.com>
2017-01-30 19:08:08 -0500

    Update parse_results.py

--- parse_results.py
+++ parse_results.py
@@ -6,13 +6,26 @@ Created on Mon Jan 30 16:08:01 2017
 """
 
 import os
+import errno
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
 
 resdir = "results"
 flines = {}
 
 lblfile = "data/coco.names"
 
-lblf = open(lblfile)
+with open(lblfile) as lblf:
+    labels = [line.rstrip('\n') for line in open(lblfile)]
+
+
 
 for fpath, subdirs, files in os.walk(resdir):
     for f in files:
@@ -59,11 +72,31 @@ with open("cartruck.train.list", "w") as tl:
     for x in flines:
         line = flines[x]
         if(line["lbl"] not in ("car", "truck") and line["wb"] < 0.4):
-            continue        
-        tl.write(line["fname"])
+            continue    
+        fname = line["fname"]
+        
+#        print labels
+        lblidx = labels.index(line["lbl"])   
+
+        
+        tgtdir, tgtf = os.path.split(fname);
+
+        # set target dir for labels        
+        if(tgtdir.startswith("..")):
+            tgtdir = tgtdir[3:]            
+        tgtdir = os.path.join("labels", tgtdir)
+        
+        #set target label and boxfile
+        tgtf, tgtfe = os.path.splitext(tgtf)        
+        tgtf = tgtf + ".txt"
+        tgtf = os.path.join(tgtdir, tgtf)
+        
+        mkdir_p(tgtdir)
+        with open(tgtf, "w") as t:
+            t.write("%s %f %f %f %f\n" % (lblidx, line["xb"], line["yb"], line ["wb"], line["hb"]))
+
+        
+        
+        tl.write(fname)
         tl.write("\n")
-
-
-
-
 
